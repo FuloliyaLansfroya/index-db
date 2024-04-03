466f7cb85253e83bd0d896b3315ccf2ef406d327
wuhe <wuhe@360.cn>
2024-04-03 19:40:30 +0800

    feat: 升级到1.0.19

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 const create = Symbol('create');
+const reset = Symbol('reset');
 type createInter = {
   storeName: string; // 数据表名称
   createType: string; // 创建方式 manual 手动 automatic 自动
@@ -10,7 +11,6 @@ class IndexDB {
   dbName: any;
   db: any | null;
   version: any;
-  dataSheet!: string[];
   isOpen: boolean;
   /**
    *
@@ -20,10 +20,8 @@ class IndexDB {
     this.dbName = name; // 数据库名称
     this.db = null; // 数据库实例
     this.version = 1; // 版本号
-    this.dataSheet = []; // 数据表列表
     this.isOpen = false; // 是否开启了数据库
   }
-
   /**
    * 打开数据库
    * @returns Promise
@@ -43,7 +41,6 @@ class IndexDB {
           this.db = null;
         } else {
           this.version = this.db.version;
-          this.dataSheet = Array.from(objectStoreNames);
           resolve(this.db);
         }
         this.isOpen = true;
@@ -62,7 +59,7 @@ class IndexDB {
       throw Error('The database is not open, this method cannot be used!');
     }
     this.db && this.db.close();
-    this.db = null;
+    this.isOpen = false;
   }
 
   /**
@@ -73,34 +70,54 @@ class IndexDB {
       throw Error('The database is not open, this method cannot be used!');
     }
     this.db && this.db.close();
-    this.db = null;
     window.indexedDB.deleteDatabase(this.dbName);
+    this[reset]();
   }
-
+  /**
+   * @function 重置数据
+   */
+  [reset] () {
+    this.dbName = ''; // 数据库名称
+    this.db = null; // 数据库实例
+    this.version = 1; // 版本号
+    this.isOpen = false; // 是否开启了数据库
+  }
+  
   /**
    * @function 新建数据表
    * @param {Array<createInter>} createArr 新建数据表数组
    * @returns Promise
    */
   createDataSheet (createArr: Array<createInter>) {
-    if (!this.isOpen) {
-      throw Error('The database is not open, this method cannot be used!');
-    }
-    if (this.db) {
-      for (let i = 0; i < createArr.length; i += 1) {
-        if (!this.dataSheet.includes(createArr[i].storeName)) {
-          this.version += 1;
-          break;
+    return new Promise((resolve, reject) => {
+      if (!this.isOpen) {
+        reject(Error('The database is not open, this method cannot be used!'));
+        return;
+      }
+      for(let  i = 0; i< createArr.length; i++) {
+        const { storeName, createType, DBType = '', primaryKey = '' } = createArr[i];
+        if (createType === 'automatic') {
+        } else if (createType === 'manual') {
+          if (!DBType || !primaryKey) {
+            reject(Error('Entered create type is manual, but Other required parameters are empty!'));
+            return;
+          }
+        } else {
+          reject(Error(`Entered create type '${createType}' is wrong value!`));
+          return;
+        }
+        if (this.db && this.db.objectStoreNames.contains(storeName)) {
+          reject(Error(`data table ${storeName} to be created already exists`))
+          return;
         }
       }
-    }
-    return new Promise((resolve, reject) => {
+      this.version += 1;
       this.db && this.db.close();
       const database = window.indexedDB.open(this.dbName, this.version);
-      this[create](database, createArr);
+      this[create](database, createArr, reject);
       database.onsuccess = (event: any) => {
         this.db = event.target.result;
-        resolve(this.db);
+        resolve('success');
       };
       database.onerror = () => {
         reject(Error("Why didn't you allow my web app to use IndexedDB?!"));
@@ -114,23 +131,17 @@ class IndexDB {
    * @param {createInter[]} createArr 新建数据表数组
    * 私有函数
    */
-  [create] (database: IDBOpenDBRequest, createArr: createInter[]) {
+  [create] (database: IDBOpenDBRequest, createArr: createInter[], reject: (reason?: any) => void ) {
     if (database) {
       database.onupgradeneeded = function (event: any) {
         const db = event.target.result;
         createArr.forEach((val) => {
           const { storeName, createType, indexArr = [], DBType = '', primaryKey = '' } = val;
           if (!db.objectStoreNames.contains(storeName)) {
-            /**
-             * automatic 自动化
-             * manual 手动化
-             */
+            /** automatic 自动化 manual 手动化 */
             if (createType === 'automatic') {
               db.createObjectStore(storeName, { autoIncrement: true });
             } else if (createType === 'manual') {
-              if (!DBType || !primaryKey) {
-                throw Error('Entered create type is manual, but Other required parameters are empty!');
-              }
               const objectStore = db.createObjectStore(storeName, { keyPath: primaryKey });
               indexArr &&
                 indexArr.length &&
@@ -140,8 +151,6 @@ class IndexDB {
               objectStore.transaction.oncomplete = function () {
                 db.transaction(storeName, DBType).objectStore(storeName);
               };
-            } else {
-              throw Error(`Entered create type '${createType}' is wrong value!`);
             }
           }
         });
@@ -156,26 +165,31 @@ class IndexDB {
    * @returns Promise
    */
   set (storeName: string, data: object[]) {
-    if (!this.isOpen) {
-      throw Error('The database is not open, this method cannot be used!');
-    }
-    const transaction = this.db.transaction([storeName], 'readwrite');
-    const objectStore = transaction.objectStore(storeName);
-    data.forEach(function (customer: any) {
-      const request = objectStore.put(customer);
-      request.onsuccess = function () {
-        // event.target.result === customer.ssn;
-      };
-    });
     return new Promise((resolve, reject) => {
-      transaction.oncomplete = function () {
-        resolve(transaction);
-      };
-
-      transaction.onerror = function () {
-        // 不要忘记错误处理！
-        reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
-      };
+      if (!this.isOpen) {
+        reject(Error('The database is not open, this method cannot be used!'));
+        return;
+      }
+      if(this.db.objectStoreNames.contains(storeName)) {
+        const transaction = this.db.transaction([storeName], 'readwrite');
+        const objectStore = transaction.objectStore(storeName);
+        data.forEach(function (customer: any) {
+          const request = objectStore.put(customer);
+          request.onsuccess = function () {
+            // event.target.result === customer.ssn;
+          };
+        });
+        transaction.oncomplete = function () {
+          resolve(transaction);
+        };
+  
+        transaction.onerror = function () {
+          // 不要忘记错误处理！
+          reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
+        };
+      } else {
+        reject(Error(`data table ${storeName} is not create`))
+      }
     });
   }
 
@@ -186,20 +200,56 @@ class IndexDB {
    * @returns Promise
    */
   delete (storeName: string, key: string) {
-    if (!this.isOpen) {
-      throw Error('The database is not open, this method cannot be used!');
-    }
-    const transaction = this.db.transaction([storeName], 'readwrite');
-    const objectStore = transaction.objectStore(storeName);
-    objectStore.delete(key);
     return new Promise((resolve, reject) => {
-      transaction.onsuccess = function () {
-        resolve(transaction);
-      };
-      transaction.onerror = function () {
-        // 不要忘记错误处理！
-        reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
-      };
+      if (!this.isOpen) {
+        reject(Error('The database is not open, this method cannot be used!'));
+        return;
+      }
+      if(this.db.objectStoreNames.contains(storeName)) {
+        const transaction = this.db.transaction([storeName], 'readwrite');
+        const objectStore = transaction.objectStore(storeName);
+        objectStore.delete(key);
+        transaction.onsuccess = function () {
+          resolve(transaction);
+        };
+        transaction.onerror = function () {
+          // 不要忘记错误处理！
+          reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
+        };
+      } else {
+        reject(Error(`data table ${storeName} is not create`))
+      }
+    });
+  }
+  /**
+   * @function 删除数据表
+   * @param {string} storeName 数据表名称
+   * @returns Promise
+   */
+  deleteObjectStore(storeName: string) {
+    return new Promise((resolve, reject) => {
+      if (!this.isOpen) {
+        reject(Error('The database is not open, this method cannot be used!'));
+        return;
+      }
+      if(this.db.objectStoreNames.contains(storeName)) {
+        this.version += 1;
+        this.db && this.db.close();
+        const database = window.indexedDB.open(this.dbName, this.version);
+        database.onsuccess = (event: any) => {
+          this.db = event.target.result;
+          resolve('success');
+        };
+        database.onupgradeneeded = function(event: any) {
+          const db = event.target.result;
+          db.deleteObjectStore(storeName); // 删除数据表
+        };
+        database.onerror = () => {
+          reject(Error("Why didn't you allow my web app to use IndexedDB?!"));
+        };
+      } else {
+        reject(Error(`data table ${storeName} is not create`))
+      }
     });
   }
 
@@ -212,33 +262,38 @@ class IndexDB {
    * @returns Promise
    */
   search (storeName: string, index: string, search: any, isVague: boolean = false) {
-    if (!this.isOpen) {
-      throw Error('The database is not open, this method cannot be used!');
-    }
-    search = search.toString().toLocaleLowerCase();
-    const transaction = this.db.transaction([storeName]);
-    const objectStore = transaction.objectStore(storeName);
-    const customers: any = [];
     return new Promise((resolve, reject) => {
-      objectStore.openCursor().onsuccess = function (event: { target: { result: any } }) {
-        const cursor = event.target.result;
-        if (cursor) {
-          if (!cursor.value[index]) {
-            throw Error(`There is no '${index}' index in the '${storeName}' data table, please check whether the value of the data is correct!`);
+      if (!this.isOpen) {
+        reject(Error('The database is not open, this method cannot be used!'));
+        return;
+      }
+      search = search.toString().toLocaleLowerCase();
+      if(this.db.objectStoreNames.contains(storeName)) {
+        const transaction = this.db.transaction([storeName]);
+        const objectStore = transaction.objectStore(storeName);
+        const customers: any = [];
+        objectStore.openCursor().onsuccess = function (event: { target: { result: any } }) {
+          const cursor = event.target.result;
+          if (cursor) {
+            if (!cursor.value[index]) {
+              reject(Error(`There is no '${index}' index in the '${storeName}' data table, please check whether the value of the data is correct!`));
+            }
+            const targetSearch = cursor.value[index] && cursor.value[index].toString().toLocaleLowerCase();
+            if ((isVague && targetSearch.includes(search)) || (!isVague && targetSearch === search)) {
+              customers.push(cursor.value);
+            }
+            cursor.continue();
+          } else {
+            resolve(customers);
           }
-          const targetSearch = cursor.value[index] && cursor.value[index].toString().toLocaleLowerCase();
-          if ((isVague && targetSearch.includes(search)) || (!isVague && targetSearch === search)) {
-            customers.push(cursor.value);
-          }
-          cursor.continue();
-        } else {
-          resolve(customers);
-        }
-      };
-      transaction.onerror = function () {
-        // 不要忘记错误处理！
-        reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
-      };
+        };
+        transaction.onerror = function () {
+          // 不要忘记错误处理！
+          reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
+        };
+      } else {
+        reject(Error(`data table ${storeName} is not create`))
+      }
     });
   }
   //  "prev"
@@ -259,27 +314,32 @@ class IndexDB {
    * @returns Promise
    */
   getOfKeyRange (storeName: string, KeyRange?: IDBKeyRange | null, desc?: string | null) {
-    if (!this.isOpen) {
-      throw Error('The database is not open, this method cannot be used!');
-    }
-    const customers: any = [];
-    const transaction = this.db.transaction([storeName]);
-    const objectStore = transaction.objectStore(storeName);
     return new Promise((resolve, reject) => {
-      const openCursor = desc ? objectStore.openCursor(KeyRange, desc) : objectStore.openCursor(KeyRange);
-      openCursor.onsuccess = function (event: { target: { result: any } }) {
-        const cursor = event.target.result;
-        if (cursor) {
-          customers.push(cursor.value);
-          cursor.continue();
-        } else {
-          resolve(customers);
-        }
-      };
-      transaction.onerror = function () {
-        // 不要忘记错误处理！
-        reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
-      };
+      if (!this.isOpen) {
+        reject(Error('The database is not open, this method cannot be used!'));
+        return;
+      }
+      if(this.db.objectStoreNames.contains(storeName)) {
+        const transaction = this.db.transaction([storeName]);
+        const customers: any = [];
+        const objectStore = transaction.objectStore(storeName);
+        const openCursor = desc ? objectStore.openCursor(KeyRange, desc) : objectStore.openCursor(KeyRange);
+        openCursor.onsuccess = function (event: { target: { result: any } }) {
+          const cursor = event.target.result;
+          if (cursor) {
+            customers.push(cursor.value);
+            cursor.continue();
+          } else {
+            resolve(customers);
+          }
+        };
+        transaction.onerror = function () {
+          // 不要忘记错误处理！
+          reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
+        };
+      } else {
+        reject(Error(`data table ${storeName} is not create`))
+      }
     });
   }
 
@@ -292,51 +352,61 @@ class IndexDB {
    * @returns Promise
    */
   updata (storeName: string, key: string, index: string, newVal: any) {
-    if (!this.isOpen) {
-      throw Error('The database is not open, this method cannot be used!');
-    }
-    const transaction = this.db.transaction([storeName], 'readwrite');
-    const objectStore = transaction.objectStore(storeName);
-    const request = objectStore.get(key);
     return new Promise((resolve, reject) => {
-      request.onerror = function () {
-        // 错误处理
-        reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
-      };
-      request.onsuccess = function (event: { target: { result: any } }) {
-        // 获取我们想要更新的数据
-        const data = event.target.result;
-        // 更新你想修改的数据
-        data[index] = newVal;
-        // 把更新过的对象放回数据库
-        const requestUpdate = objectStore.put(data);
-        requestUpdate.onerror = function () {
+      if (!this.isOpen) {
+        reject(Error('The database is not open, this method cannot be used!'));
+        return;
+      }
+      if(this.db.objectStoreNames.contains(storeName)) {
+        const transaction = this.db.transaction([storeName], 'readwrite');
+        const objectStore = transaction.objectStore(storeName);
+        const request = objectStore.get(key);
+        request.onerror = function () {
           // 错误处理
           reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
         };
-        requestUpdate.onsuccess = function () {
-          // 完成，数据已更新！
-          resolve(request.result);
+        request.onsuccess = function (event: { target: { result: any } }) {
+          // 获取我们想要更新的数据
+          const data = event.target.result;
+          // 更新你想修改的数据
+          data[index] = newVal;
+          // 把更新过的对象放回数据库
+          const requestUpdate = objectStore.put(data);
+          requestUpdate.onerror = function () {
+            // 错误处理
+            reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
+          };
+          requestUpdate.onsuccess = function () {
+            // 完成，数据已更新！
+            resolve(request.result);
+          };
         };
-      };
+      } else {
+        reject(Error(`data table ${storeName} is not create`))
+      }
     });
   }
 
   clear (storeName: string) {
-    if (!this.isOpen) {
-      throw Error('The database is not open, this method cannot be used!');
-    }
-    const transaction = this.db.transaction([storeName], 'readwrite');
-    const objectStore = transaction.objectStore(storeName);
-    objectStore.clear();
     return new Promise((resolve, reject) => {
-      transaction.oncomplete = function () {
-        resolve(transaction);
-      };
-      transaction.onerror = function () {
-        // 不要忘记错误处理！
-        reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
-      };
+      if (!this.isOpen) {
+        reject(Error('The database is not open, this method cannot be used!'));
+        return;
+      }
+      if(this.db.objectStoreNames.contains(storeName)) {
+        const transaction = this.db.transaction([storeName], 'readwrite');
+        const objectStore = transaction.objectStore(storeName);
+        objectStore.clear();
+        transaction.oncomplete = function () {
+          resolve(transaction);
+        };
+        transaction.onerror = function () {
+          // 不要忘记错误处理！
+          reject(Error('An error occurred during this operation, please check whether the parameters are correct!'));
+        };
+      } else {
+        reject(Error(`data table ${storeName} is not create`))
+      }
     });
   }
 }
